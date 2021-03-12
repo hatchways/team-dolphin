beda4a5b6026355f455ca47cc2796bfcb2bc7f1c
Jovialiste82 <52076767+Jovialiste82@users.noreply.github.com>
2021-03-12 09:54:52 -0400

    adding user ref

--- server/models/mentionModel.js
+++ server/models/mentionModel.js
@@ -1,37 +1,35 @@
 const mongoose = require("mongoose");
 
-const mentionSchema = mongoose.Schema(
-    {
-      company: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Company',
-      },
-      content: {
-        type: String,
-        required: true,
-      },
-      title: {
-        type: String,
-        required: true,
-      },
-      platform: {
-        type: String,
-        required: true,
-      },
-      image: {
-        type: String,
-        // We might want to add a default ?
-      },
-      date: {
-        type: Date,
-        required: true,
-      },
-      popularity: {
-        type: Number,
-        default: 0,
-      },
-    }
-  );
+const mentionSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  platform: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    // We might want to add a default ?
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  popularity: {
+    type: Number,
+    default: 0,
+  },
+});
 
-  module.exports = mongoose.model('Mention', mentionSchema);
+module.exports = mongoose.model("Mention", mentionSchema);
