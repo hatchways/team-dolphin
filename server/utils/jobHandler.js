c42298e2c6cdc2cff94ab1bc4d39b1cf120d3b5f
Guohao Ouyang <xou96@my.yorku.ca>
2021-03-24 10:10:08 -0400

    fix async problem

--- server/utils/jobHandler.js
+++ server/utils/jobHandler.js
@@ -3,54 +3,51 @@ const Queue = require("bull");
 const User = require("../models/userModel");
 const Mention = require("../models/mentionModel");
 const { addMentionsToDB } = require("./scraper");
+const { collection } = require("../models/mentionModel");
 
-const connectRedis = async () => {
-  try {
-    // to start redis server in background
-    // run "redis-server --daemonize yes"
-    // to stop the server running "redis-cli shutdown"
-    const client = redis.createClient({
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
-    });
-    client.on("error", (err) => {
-      console.log("Error " + err);
-    });
+const connectRedis = () => {
+  // to start redis server in background
+  // run "redis-server --daemonize yes"
+  // to stop the server running "redis-cli shutdown"
+  const client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  });
+  client.on("error", (err) => {
+    console.log("Error " + err);
+  });
 
-    client.on("ready", function () {
-      console.log("redis is running");
-    });
-  } catch (err) {
-    console.log(err);
-  }
+  client.on("ready", function () {
+    console.log("redis is running");
+  });
 };
 
-const handleTaskQueues = async () => {
-  try {
-    const companiesQueue = new Queue("queue-for-getting-componies", {
-      redis: { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST },
-    });
+const handleTaskQueues = () => {
+  const companiesQueue = new Queue("queue-for-getting-componies", {
+    redis: { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST },
+  });
 
-    const scrapingQueue = new Queue("queue-for-scraping", {
-      redis: { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST },
-    });
-    // job producer, repeated every 15 mins
-    companiesQueue.add(
-      {},
-      {
-        repeat: { cron: "*/15 * * * *" },
-        delay: 2000,
-        jobId: "repeatCompaniesUpdate",
-      }
-    );
+  const scrapingQueue = new Queue("queue-for-scraping", {
+    redis: { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST },
+  });
+  // job producer, repeated every 15 mins
+  companiesQueue.add(
+    {},
+    {
+      repeat: { cron: "*/15 * * * *" },
+      delay: 2000,
+      jobId: "repeatCompaniesUpdate",
+    }
+  );
 
-    // to remove existedQueue use
-    // companiesQueue.empty();
+  // to remove existedQueue use
+  // companiesQueue.empty();
 
-    // job consumer
-    companiesQueue.process(function (job, done) {
+  // job consumer
+  companiesQueue.process(async function (job, done) {
+    try {
       // delete all Mention at the first of updating job queues
-      Mention.deleteMany();
+      await Mention.deleteMany();
       User.getAllCompanies().then((companies) =>
         companies.forEach((company) => {
           for (var platform in User.schema.obj.platforms) {
@@ -60,18 +57,23 @@ const handleTaskQueues = async () => {
       );
       // call done when finished
       done();
-    });
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
-    scrapingQueue.process(function (job, done) {
-      addMentionsToDB(job.data.company, job.data.platform);
-      done();
-      console.log(
-        `Job with company ${job.data.company}, platform ${job.data.platform} has been finished`
+  scrapingQueue.process(async function (job, done) {
+    try {
+      await addMentionsToDB(job.data.company, job.data.platform);
+      done(
+        console.log(
+          `Job with company ${job.data.company}, platform ${job.data.platform} has been finished`
+        )
       );
-    });
-  } catch (err) {
-    console.log(err);
-  }
+    } catch (err) {
+      console.log(err);
+    }
+  });
 };
 
 module.exports = { connectRedis, handleTaskQueues };
--- server/utils/scraper.js
+++ server/utils/scraper.js
@@ -8,18 +8,26 @@ const addMentionsToDB = async (company, platform) => {
       case "reddit":
         const posts = await searchRecursive(company);
         await Mention.insertMany(posts);
+        console.log(`${platform} is completed`);
+        break;
       case "twitter":
-
+        console.log(`${platform} is under construction`);
+        break;
       case "facebook":
-
+        console.log(`${platform} is under construction`);
+        break;
       case "amazon":
-
+        console.log(`${platform} is under construction`);
+        break;
       case "forbes":
-
+        console.log(`${platform} is under construction`);
+        break;
       case "shopify":
-
+        console.log(`${platform} is under construction`);
+        break;
       case "businessinsider":
-
+        console.log(`${platform} is under construction`);
+        break;
       default:
         console.log(`oops! ${platform} is not inside our platform`);
     }
