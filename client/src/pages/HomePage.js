50e46d155d78731f9d6ea72fa568278fccbf5f90
nalanart <alantran96@gmail.com>
2021-03-26 09:31:42 -0400

    Add dispatch to dependency array

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -45,7 +45,7 @@ const HomePage = () => {
         setCurrentPage(1);
       })
       .catch((err) => alert("Cookie expired. Please log in again"));
-  }, [searchTerm, user.platforms]);
+  }, [searchTerm, user.platforms, dispatch]);
 
   if (mentionDatas === null) return <Spinner />;
 
